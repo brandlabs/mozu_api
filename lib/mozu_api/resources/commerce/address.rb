61eab3e5892e84615c6c026bcd8aaafa97248c9d
Carson Reinke <carson@reinke.co>
2013-10-01 08:58:02 -0400

    Added URL for address validation

--- lib/mozu_api/resources/commerce/address.rb
+++ lib/mozu_api/resources/commerce/address.rb
@@ -4,6 +4,7 @@ module MozuApi
       self.prefix = '/api/commerce/'
       
       class << self
+        #POST /commerce/customer/addressvalidation/
         def validate(address)
           raise ArgumentError unless address.is_a?(Address)
           #response = post(:"../customer/addressvalidation/", {}, {'addressValidationRequest' => address}.to_json())
